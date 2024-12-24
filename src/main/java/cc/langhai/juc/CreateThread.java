14c3f7ccc5221d50d26fce47d67c7de3a2a9e8f0
langhai <648798558@qq.com>
2024-12-24 23:26:28 +0800

    feat: 创建线程

--- src/main/java/cc/langhai/juc/CreateThread.java
+++ src/main/java/cc/langhai/juc/CreateThread.java
@@ -2,6 +2,9 @@ package cc.langhai.juc;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * 创建线程方式
  *
@@ -11,8 +14,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j(topic = "c.CreateThread")
 public class CreateThread {
     public static void main(String[] args) {
-        test02();
-        log.debug("running...");
+        FutureTask<Integer> integerFutureTask = test03();
+        try {
+            Integer integer = integerFutureTask.get();
+            log.debug("计算结果：{}", integer);
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        log.debug("main running...");
     }
 
     /**
@@ -43,5 +52,27 @@ public class CreateThread {
 
         Thread thread = new Thread(runnable, "test02");
         thread.start();
+
+        // lambda形式
+        Thread threadLambda = new Thread(() -> {
+            log.debug("running...");
+        }, "test02Lambda");
+        threadLambda.start();
+    }
+
+    /**
+     * 创建线程第三种方式：使用Callable接口
+     */
+    public static FutureTask<Integer> test03() {
+        FutureTask<Integer> integerFutureTask = new FutureTask<>(() -> {
+            log.debug("callable 计算中...");
+            Thread.sleep(2000);
+            return 100;
+        });
+
+        Thread thread = new Thread(integerFutureTask, "test03");
+        thread.start();
+
+        return integerFutureTask;
     }
 }
